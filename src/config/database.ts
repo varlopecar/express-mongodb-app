f0e71ed86dd1e432936671697677c728333956e1
varlopecar <varlopecar@gmail.com>
2025-07-08 01:49:51 +0200

    fix deploy

--- src/config/database.ts
+++ src/config/database.ts
@@ -17,7 +17,6 @@ const connectDB = async (): Promise<void> => {
       maxPoolSize: 10, // Maintain up to 10 socket connections
       serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
       socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-      bufferMaxEntries: 0, // Disable mongoose buffering
       bufferCommands: false, // Disable mongoose buffering
     };
 
